1812342fe8e31fa64b191eea97342cdceb10853d
Smurph <deranged40@gmail.com>
2016-08-31 19:33:42 -0400

    added drag with mouse

---
+++ Assets/Scripts/DragWithMouse.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragWithMouse : MonoBehaviour {
+
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        offset = transform.position - Camera.main.ScreenToWorldPoint(getMousePosition());
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(getMousePosition()) + offset;
+    }
+
+    private Vector3 getMousePosition()
+    {
+        return new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+    }
+}
